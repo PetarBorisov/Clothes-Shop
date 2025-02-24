d6ddf8efe63d179545e0245e7e87f16d0596a68e
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2025-02-24 21:03:48 +0200

    Create UserRepository.java

---
+++ Clothes_Shop/src/main/java/com/example/clothes_shop/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.clothes_shop.repository;
+
+import com.example.clothes_shop.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+
+    Optional<UserEntity> findByEmail(String email);
+}
