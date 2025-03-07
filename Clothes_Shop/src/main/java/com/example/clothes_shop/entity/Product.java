7fdf38b40f3d22fa03dafba78438fea54d9f6549
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2025-03-07 17:32:44 +0200

    Create Product.java

---
+++ Clothes_Shop/src/main/java/com/example/clothes_shop/entity/Product.java
@@ -0,0 +1,9 @@
+package com.example.clothes_shop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity{
+}
