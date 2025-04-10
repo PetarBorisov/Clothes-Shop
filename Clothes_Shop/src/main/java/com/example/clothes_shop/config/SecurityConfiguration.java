614ec7c9e0f2cc98625d17d226a3daa32ad31ae1
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2025-04-10 23:43:56 +0300

    Update SecurityConfiguration.java

--- Clothes_Shop/src/main/java/com/example/clothes_shop/config/SecurityConfiguration.java
+++ Clothes_Shop/src/main/java/com/example/clothes_shop/config/SecurityConfiguration.java
@@ -26,65 +26,43 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity
-
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/delete/category/**"))  // Игнориране на CSRF за изтриване на категорията
                 .authorizeHttpRequests(auth -> auth
-                        // Define which urls are visible by which users
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/comments/delete/{id}/**").hasRole("ADMIN")
-                        // All static resources which are situated in js, images, css are available for anyone
+                        .requestMatchers("/delete/category/**").hasRole("ADMIN")  // Ограничаваме изтриването на категория само за администратори
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        // Allow anyone to see the home page, the registration page and the login form
                         .requestMatchers( "/","/users/login", "/users/register", "/users/login-error", "/css/style.css").permitAll()
                         .requestMatchers("/api/comments/add","/api/comments/view").permitAll()
-                        // all other requests are authenticated.*/
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 ).formLogin(
-                        formLogin -> {
-                            formLogin
-                                    // redirect here when we access something which is not allowed.
-                                    // also this is the page where we perform login.
-                                    .loginPage("/users/login")
-                                    // The names of the input fields (in our case in auth-login.html)
-                                    .usernameParameter("email")
-                                    .passwordParameter("password")
-                                    .defaultSuccessUrl("/",true)
-                                    .failureForwardUrl("/users/login-error");
-                        }
-
+                        formLogin -> formLogin
+                                .loginPage("/users/login")
+                                .usernameParameter("email")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true)
+                                .failureForwardUrl("/users/login-error")
                 ).logout(
-                        logout -> {
-                            logout
-                                    // the URL where we should POST something in order to perform the logout
-                                    .logoutUrl("/users/logout")
-                                    // where to go when logged out?
-                                    .logoutSuccessUrl("/")
-                                    // invalidate the HTTP session
-                                    .invalidateHttpSession(true);
-                        }
-
+                        logout -> logout
+                                .logoutUrl("/users/logout")
+                                .logoutSuccessUrl("/")
+                                .invalidateHttpSession(true)
                 ).rememberMe(
-                        rememberMe -> {
-                            rememberMe
-                                    .key(rememberMeKey)
-                                    .rememberMeParameter("rememberme")
-                                    .rememberMeCookieName("rememberme");
-                        }
+                        rememberMe -> rememberMe
+                                .key(rememberMeKey)
+                                .rememberMeParameter("rememberme")
+                                .rememberMeCookieName("rememberme")
                 ).build();
-
     }
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
-        // This service translates the restaurant users and roles
-        // to representation which spring security understands.
         return new ClothesShopUserDetailsService(userRepository);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-
     }
 }
